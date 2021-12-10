b6fe4180728995582c2d4463e958ef11b26fe150
gaktauahmalesd <95903648+gaktauahmalesd@users.noreply.github.com>
2021-12-10 19:29:26 +0700

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -94,8 +94,9 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
-			menuItem.screenCenter(X);
-			menuItems.add(menuItem);
+			// menuItem.screenCenter(X);
+			 menuItem.x += 390;
+            menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
 			menuItem.scrollFactor.set(0, scr);
@@ -241,7 +242,7 @@ class MainMenuState extends MusicBeatState
 
 		menuItems.forEach(function(spr:FlxSprite)
 		{
-			spr.screenCenter(X);
+			// spr.screenCenter(X);
 		});
 	}
 
